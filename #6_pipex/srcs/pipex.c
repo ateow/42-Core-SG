fc852e1806aca3f4d3a24d3da771c45cdbcee975
Aaron Teow <ateow@c2r1s8.42singapore.sg>
2023-10-17 00:21:58 +0800

    updated

--- #6_pipex/srcs/pipex.c
+++ #6_pipex/srcs/pipex.c
@@ -49,40 +49,66 @@ int	main(int argc, char **argv, char *envp[])
 	int		file;
 	t_list	input1;
 	t_list	input2;
-
-	//input1.cmd = get_args(argv[1], argv[2]); // for execve arguments
-    	input1.cmd = ft_split(argv[2], ' ');	
-	input1.bin_path = get_pathname(envp, input1.cmd[0]); // for execve binpath
-    	input2.cmd = ft_split(argv[3], ' ');
+	
+	if (argc < 4)
+		return (1);
+	input1.cmd = ft_split(argv[2], ' ');
+	input2.cmd = ft_split(argv[3], ' ');
+	input1.bin_path = get_pathname(envp, input1.cmd[0]);
 	input2.bin_path = get_pathname(envp, input2.cmd[0]);
-	//printf("input.cmd:%s\n", input1.cmd[2]);
-	pipe(fd);
+	if (pipe(fd) == -1)
+		exit (-1);
 	child_pid = fork();
 	if (child_pid == 0)
 	{
 		file = open(argv[1], O_RDONLY);
+		if (file == -1)
+			return (1);
 		dup2(file, STDIN_FILENO);
 		close(file);
 		
 		close(fd[0]);
 		dup2(fd[1], STDOUT_FILENO);
 		i = 0;
-		while (execve(input1.bin_path[i], input1.cmd, NULL) == -1)
+		while (input1.bin_path[i] != NULL)
+		{
+			if (access(input1.bin_path[i], F_OK) != -1 && access(input1.bin_path[i], X_OK) != -1)
+			{
+				if (execve(input1.bin_path[i], input1.cmd, NULL) == -1)
+					perror("execve");
+				else
+					break ;
+			}
 			i++;
-		perror("execve");
+		}
+		if (input1.bin_path[i] == NULL)
+			perror(input1.cmd[0]);
 	}
 	else
 	{
-		file = open(argv[argc - 1], O_WRONLY | O_CREAT, 0777);
+		//if (access(argv[argc - 1], F_OK) == 0)
+		//	unlink(argv[argc - 1]);
+		file = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+		if (file == -1)
+			return (1);
 		dup2(file, STDOUT_FILENO);
 		close(file);
-			
 		close(fd[1]);
 		dup2(fd[0], STDIN_FILENO);
 		i = 0;
-    		while (execve(input2.bin_path[i], input2.cmd, NULL) == -1)
-    			i++;
-    		perror("execve");
+		while (input2.bin_path[i] != NULL)
+		{
+			if (access(input2.bin_path[i], F_OK) != -1 && access(input2.bin_path[i], X_OK) != -1)
+			{
+				if (execve(input2.bin_path[i], input2.cmd, NULL) == -1)
+					perror("execve");
+				else
+					break ;
+			}
+			i++;
+		}
+		if (input2.bin_path[i] == NULL)
+			perror(input2.cmd[0]);
 	}
 	close(fd[0]);
 	close(fd[1]);
@@ -90,7 +116,6 @@ int	main(int argc, char **argv, char *envp[])
 	free_struct(input2);
 	return (0);
 }
-
 
 /*
 char	*get_read_string(char *file) //extract content from file1
