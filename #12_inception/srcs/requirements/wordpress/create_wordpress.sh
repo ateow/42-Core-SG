0e36998487d575e0510fdf294cd6ea9a42344aa5
kali <kali@kali.(none)>
2024-04-07 02:04:38 +0800

    initalise inception

---
+++ #12_inception/srcs/requirements/wordpress/create_wordpress.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#check if wp-config.php exist
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress present"
+else
+	cd /var/www/html
+
+	#Download wordpress and all config file
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
+
+	#Inport env variables in the config file
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
+fi
+
+exec "$@"
