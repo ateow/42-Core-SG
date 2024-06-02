68cd176aea9d6e0b36f2bba7d6ad095575433e66
kali <kali@kali.(none)>
2024-06-03 00:44:05 +0800

    updates

--- #11_C++_modules/C++_Module_06/ex00/main.cpp
+++ #11_C++_modules/C++_Module_06/ex00/main.cpp
@@ -6,70 +6,19 @@
 /*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 16:43:16 by kali              #+#    #+#             */
-/*   Updated: 2024/06/01 23:21:06 by kali             ###   ########.fr       */
+/*   Updated: 2024/06/03 00:43:59 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "Bureaucrat.hpp"
+#include "ScalarConverter.hpp"
 
-int main()
+int main(int argc, char *argv[]) 
 {
-    std::cout << "\n=== initi grade too low exception ===\n";
-    try
-    {
-        Bureaucrat farmer("farmer", 200);
-        farmer.demote_Grade(); 
-        // exception is thrown during the constructing
-        // still constructing, hence destructor will not be called as it
-    }
-    catch (std::exception& e)
-    {
-        std::cout << e.what() << std::endl;   
-    }
-
-    std::cout << "\n=== grade too low exception ===\n";
-    try
-    {
-        Bureaucrat farmer("farmer", 150);
-        farmer.demote_Grade(); // when an exception happens, destructor is called immediately
-        Bureaucrat scholar("scholar", 1); // this will not run because an exception is thrown
-        scholar.promote_Grade();
-    }
-    catch (std::exception& e)
-    {
-        std::cout << e.what() << std::endl;   
-    }
-
-    std::cout << "\n=== grade too high exception ===\n";
-    try
-    {
-        Bureaucrat scholar("scholar", 1); 
-        scholar.promote_Grade(); // this will not run because an exception is thrown
-        Bureaucrat farmer("farmer", 150);// when an exception happens, destructor is called immediately
-        farmer.demote_Grade(); 
-    }
-    catch (std::exception& e)
-    {
-        std::cout << e.what() << std::endl;   
-    }
-
-    std::cout << "\n=== initi and cout bureaucrat class ===\n";
-    Bureaucrat* aaron = new Bureaucrat("aaron", 9);
-    // Bureaucrat aaron("aaron", 9); // grade 9
-    std::cout << *aaron << std::endl;
-    aaron->promote_Grade(); // grade 8
-    std::cout << *aaron << std::endl;
-    aaron->demote_Grade(); // grade 9
-    std::cout << *aaron << std::endl;
-    delete aaron;
-
-    std::cout << "\n=== initi copy constructor and assigment ===\n";
-
-    Bureaucrat orginal("orginal", 88);
-    Bureaucrat copy(orginal); // copy grade is 88
-    std::cout << copy <<std::endl; 
-    Bureaucrat orginal2("orginal2", 5);
-    copy = orginal2; // copy grade is now 5. name remain same because its' constant
-    std::cout << copy <<std::endl;
+	if (argc != 2) 
+	{
+		std::cout << "Error: no parameters\n" << "Usage: ./convert <literal>" << std::endl;
+		return 1;
+	}
+	ScalarConverter::convert(argv[1]);
+	return 0;
 }
-
